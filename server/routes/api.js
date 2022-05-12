64b6413cbfadc5b81502c4c3ae4936ca88e68f36
Tyler Eggert <tyler.a.eggert@gmail.com>
2022-05-12 16:23:28 -0500

    Added locations route to backend

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,7 @@ const { BASE_URL } = require('../index')
 const aircraftStatus = require('./aircraftStatus');
 const flyingSchedule = require('./flyingSchedule');
 const imds = require('./imds');
+const locations = require('./locations');
 const notes = require('./notes');
 const scheduledMx = require('./scheduledMx');
 
@@ -15,6 +16,7 @@ router.get('/', (req, res, next) => {
     aircraft_status: `${BASE_URL}/aircraft_status`,
     flying_schedule: `${BASE_URL}/flying_schedule`,
     imds: `${BASE_URL}/imds`,
+    locations: `${BASE_URL}/locations`,
     notes: `${BASE_URL}/notes`,
     scheduled_mx: `${BASE_URL}/scheduled_mx`
   });
@@ -26,6 +28,7 @@ router.all('/', (req, res) => {
 
 router.use('/aircraft_status', aircraftStatus);
 router.use('/imds', imds);
+router.use('/locations', locations);
 router.use('/flying_schedule', flyingSchedule);
 router.use('/notes', notes);
 router.use('/scheduled_mx', scheduledMx);
